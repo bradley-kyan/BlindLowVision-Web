5d03ef88fcd460a599b15c4af2f543cfedb13eb8
Kyan Bradley <31454939+bradley-kyan@users.noreply.github.com>
2020-10-01 09:32:04 +1300

    Cleanup, removal of uneccecary components
    Removed composer fromsolution including sendgrid api.
    
    Changed email to a downloaded jpeg which contains the qr code -> due to linitations regarding email server and lack of ssl (sendgrid).
      => Jpeg used though html2canvas javascript component.
    
    Removed sendqr.php

---
+++ qrget.php
@@ -0,0 +1,18 @@
+<?php
+
+if(!isset($_COOKIE['cookieUserLogin'])) {
+  //cookie does not exist;
+    echo 404;
+    exit;
+}
+
+$cookieGet = $_COOKIE['cookieUserLogin'];
+$cookieData = json_decode($cookieGet, true);
+
+//UserId from login cookie
+$email = $cookieData['Email'];
+$name1 = $cookieData['first_name'];
+$UserID = $cookieData['UserID'];
+echo $UserID;
+
+?>
