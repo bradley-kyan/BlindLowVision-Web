c1331a8402cebfdaa1f557a50f69dac38113ebfc
Kyan Bradley <31454939+bradley-kyan@users.noreply.github.com>
2020-10-04 00:06:05 +1300

    Application Php
    Verification of user from mobile aplication

---
+++ appLogin.php
@@ -0,0 +1,39 @@
+<?php
+$usernameLogin = $_POST['Username'];
+$passwordLogin = $_POST['Password'];
+
+    /*Connect using SQL Server authentication.*/  
+$serverName = "tcp:blv2020.database.windows.net,1433";  
+$connectionOptions = array(  
+    "Database" => "blvuserdata",  
+    "UID" => "connection",  
+    "PWD" => "blv2020@connecti0n"  
+);  
+$conn = sqlsrv_connect($serverName, $connectionOptions);  
+      
+    $Sql = "
+    DECLARE @ID varchar(255)
+    SET @ID = (SELECT Username FROM SupportUser WHERE Username='$usernameLogin' AND Password = '$passwordLogin');
+
+    IF @ID IS NULL
+        BEGIN
+            SELECT '404' AS 'Status'
+            SET NOEXEC ON
+        END
+    SELECT '200' AS 'Status';
+    ";
+    $stmt = sqlsrv_query($conn, $Sql, $params);
+    $row_count = sqlsrv_has_rows( $stmt );
+    $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC);
+
+
+    
+if ($row_count === false){
+        echo 404;
+        exit;
+        }    
+    else{
+        echo $row['Status'];
+        exit;
+    }
+    ?>
